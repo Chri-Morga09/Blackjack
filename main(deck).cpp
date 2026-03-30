9dda5def87ce906e81adc843c077fd9720d6543f
alessandro <alematrone279@gmail.com>
2026-03-30 18:04:02 +0200

    Add files via upload

---
+++ main(deck).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "Deck.h"
+
+using namespace std;
+
+//0 fiori 
+//1 cuori 
+//2 picche
+//3 quadri 
+
+int main() {
+    // Usa una conversione esplicita per evitare avvisi di perdita di dati
+    srand(time(NULL));
+
+    Deck mazzo;
+
+    cout << "TEST CLASSE DECK" << endl;
+    cout << "carte iniziali: " << mazzo.cardsLeft() << endl;
+
+    // Pesca 5 carte di prova
+    for (int i = 0; i < 5; ++i) {
+        Card c = mazzo.drawCard();
+        cout << "carta " << i + 1 << ": valore " << c.getRank() << " | seme " << c.getSuit() << endl;
+    }
+
+    cout << "carte rimaste dopo la pesca: " << mazzo.cardsLeft() << endl;
+
+    // Test del reset
+    mazzo.reset();
+    cout << "mazzo resettato. carte disponibili: " << mazzo.cardsLeft() << endl;
+
+    return 0;
+}
