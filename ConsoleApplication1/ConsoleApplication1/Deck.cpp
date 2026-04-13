395611a2ffbc3c441c8e5630cf60a4cf9dfa1726
alem-279 <alematrone279@gmail.com>
2026-04-13 20:33:18 +0200

    Refactor Deck class methods and variable names

--- ConsoleApplication1/ConsoleApplication1/Deck.cpp
+++ ConsoleApplication1/ConsoleApplication1/Deck.cpp
@@ -1,49 +1,50 @@
 #include "Deck.h"
-#include <cstdlib> // Per rand()
+#include <ctime>
 
-// Costruttore
+srand(time(NULL));
+
 Deck::Deck() {
     reset();
 }
 
-// Crea le 52 carte standard
-void Deck::generate() {
-    int count = 0;
-    for (int s = 0; s < 4; s++) {
-        for (int r = 1; r <= 13; r++) {
-            this->cards[count] = Card(s, r); 
-            count++;
+// crea le 52 carte standard
+void Deck::generaMazzo() {
+    int contatore = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 1; j <= 13; j++) {
+            this->carte[contatore] = Card(i, j); 
+            contatore++;
         }
     }
-    this->topCardIndex = 0;
+    this->indiceProxCarta = 0;
 }
 
-// Algoritmo di rimescolamento manuale
-void Deck::shuffle() {
+// rimescola carte
+void Deck::mescola() {
     for (int i = 51; i > 0; i--) {
         int j = rand() % (i + 1);
-        Card temp = this->cards[i];
-        this->cards[i] = this->cards[j];
-        this->cards[j] = temp;
+        Card temp = this->carte[i];
+        this->carte[i] = this->carte[j];
+        this->carte[j] = temp;
     }
-    this->topCardIndex = 0; // Reset dell'indice dopo aver mischiato
+    this->indiceProxCarta = 0; // reset dell'indice dopo aver mischiato
 }
 
-// Pesca la carta in cima
-Card Deck::drawCard() {
-    if (this->topCardIndex >= 52) {
-        reset(); // Se finiscono, ricomincia
+// pesca la carta in cima
+Card Deck::pescaCarta() {
+    if (this->indiceProxCarta >= 52) {
+        reset(); // se finiscono ricomincia
     }
-    return this->cards[topCardIndex++];
+    return this->carte[indiceProxCarta++];
 }
 
 // Calcola quante carte restano
-int Deck::cardsLeft() {
-    return 52 - this->topCardIndex;
+int Deck::carteRimanenti() {
+    return 52 - this->indiceProxCarta;
 }
 
 // Reset totale
 void Deck::reset() {
-    generate();
-    shuffle();
+    generaMazzo();
+    mescola();
 }
