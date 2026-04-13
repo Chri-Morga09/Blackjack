d247a6b58795cb9f969b96571939ed94553ceb2f
alem-279 <alematrone279@gmail.com>
2026-04-13 20:57:09 +0200

    miglioramento card #

--- ConsoleApplication1/ConsoleApplication1/Card.cpp
+++ ConsoleApplication1/ConsoleApplication1/Card.cpp
@@ -2,22 +2,70 @@
 
 //costruttore vuoto: inizializza carta con valori di default
 Card::Card() {
-    this->suit = 0;  // seme di default
+
+    this->suit = 0;  //seme di default
     this->rank = 1;  // valore di default (asso)
 }
 
 //costruttore con parametri: inizializza carta con seme e valore scelti
 Card::Card(int s, int r) {
+
     this->suit = s;
     this->rank = r;
 }
 
 //restituisce il seme della carta
 int Card::getSuit() {
+
     return this->suit;
 }
 
 //restituisce il valore della carta
 int Card::getRank() {
+
     return this->rank;
+}
+
+//imposta il sme;
+void Card::setSuit(int s){
+
+    this->suit = s;
+}
+
+//imposta il valore
+void Card::setRank(int r){
+
+    this->suit = r;
+}
+
+
+string Card::toString(){
+
+    string semi[4];
+
+    semi[0] = "Cuori";
+    semi[1] = "Quadri";
+    semi[2] = "Fiori";
+    semi[3] = "Picche";
+
+    string valori[14];
+
+    valori[0] = "";
+    valori[1] = "Asso";
+    valori[2] = "2";
+    valori[3] = "3";
+    valori[4] = "4";
+    valori[5] = "5";
+    valori[6] = "6";
+    valori[7] = "7";
+    valori[8] = "8";
+    valori[9] = "9";
+    valori[10] = "10";
+    valori[11] = "Jack";//box
+    valori[12] = "Quen";
+    valori[13] = "Re";
+    
+
+    string risultato = valori[this->rank] + " di " + semi[this->suit];
+    return risultato;
 }
--- ConsoleApplication1/ConsoleApplication1/Card.h
+++ ConsoleApplication1/ConsoleApplication1/Card.h
@@ -1,16 +1,21 @@
 #pragma once
+#include <string>
+using namespace std;
 
 class Card {
 
 private:
-
-    int suit;  // seme della carta
-    int rank;  // valire della carta
+     int suit; //seme della carta
+    int rank;  //valire della carta
 
 public:
-    Card();                  // costruttore vuoto che crea carta di default
-    Card(int s, int r);      // costruttore con parametri vche crea carta specifica
+    Card();  //costruttore vuoto che crea carta di default
+    Card(int s, int r); //costruttore con parametri vche crea carta specifica
 
-    int getSuit();           // restituisce il seme
-    int getRank();           // restituisce il valorw
+    int getSuit();//restituisce il seme
+    int getRank();//restituisce il valorw
+    void setSuit(int s); //imposta il seme
+    void setRank(int r);//imposta il valore
+
+    string toString();
 };
